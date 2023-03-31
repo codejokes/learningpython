59ae3dddb94497776c8ff924492cd8a65ecaf14a
Diego Urdanigo <diegourdanigo@gmail.com>
2023-03-30 22:26:50 -0500

    se va la calculadora

---
+++ calculadorapython.py
@@ -0,0 +1,20 @@
+def suma(a, b):
+    respuesta = a + b
+    print(str(a) +" + "+ str(b) +" = "+ str(respuesta))
+
+
+def resta(a,b):
+    respuesta = a - b
+    print(str(a) +" - "+ str(b) +" = "+str(respuesta))
+
+
+def multiplicacion(a, b):
+    respuesta = a  * b
+    print(str(a) +" * "+ str(b) +" = "+ str(respuesta))
+
+def division(a, b):
+    respuesta = a / b
+    print(str(a) +"/"+ str(b) +"="+ str(respuesta))
+
+
+
